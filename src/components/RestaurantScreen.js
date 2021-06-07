997995e2e8e77ed33e6a960080bc1013062bdf8c
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-06-06 21:05:39 -0700

    style app with material design

--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -1,7 +1,14 @@
-const RestaurantScreen = () => (
-  <div>
-    <h1>Restaurants</h1>
-  </div>
-);
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import RestaurantList from './RestaurantList';
 
+const RestaurantScreen = () => (
+  <Card>
+    <CardContent>
+      <Typography variant="h5">Restaurants</Typography>
+      <RestaurantList />
+    </CardContent>
+  </Card>
+);
 export default RestaurantScreen;
